09da6c674c1628f652cecbb4e50730ff0602dd78
Bryan <bfischer@zagmail.gonzaga.edu>
2023-04-14 14:45:41 -0700

    updated BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,3 +1,20 @@
 #include "BST.h"
 
 //you write
+
+void BST::prettyPrint(){
+    prettyPrint(root, "+");
+}
+
+void BST::prettyPrint(const Node* node, std::string str){
+    if(node == nullptr){
+        std::cout << str << "\\[nullptr]" << std::endl;
+        return;
+    }else{
+        std::cout << str << "\\[" << node->value << "]" << std::endl;
+        str += "-";
+        prettyPrint(node->leftChild, str);
+        prettyPrint(node->rightChild, str);
+    }
+
+}
